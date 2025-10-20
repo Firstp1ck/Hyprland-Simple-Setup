dd7fb469202b3ef8bb24dbc7705293bdea231e52
Firstp1ck <al.leuzi@hotmail.com>
2025-10-20 03:05:27 +0200

    fix install sections

--- setup.sh
+++ setup.sh
@@ -1867,7 +1867,7 @@ enable_sddm_last() {
 
 # Verify configs do not pre-create workspace 11
 verify_workspace_config() {
-    print_message "Verifying workspace"
+    announce_step "Verifying workspace"
     local issues=0
     local files=(
         "$HOME/.config/hypr/sources/monitors.conf"
